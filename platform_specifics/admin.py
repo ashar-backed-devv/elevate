849a7d57033e683262aeef27fc82ebf65ba3101f
ashar-backed-devv <arqamahmad805@gmail.com>
2025-11-13 01:05:04 +0500

    Delete platform_specifics directory

--- platform_specifics/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import HelpCenter, Blog, BlogReply, NewsletterSubscription, GeneralAnnouncement, HeroSection
-
-# Register your models here.
-admin.site.register([HelpCenter, Blog, BlogReply, NewsletterSubscription, GeneralAnnouncement, HeroSection])
--- platform_specifics/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlatformSpecificsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'platform_specifics'
