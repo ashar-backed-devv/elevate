12714ce912432cff589d0122aec490fb64a27fb1
ashar-backed-devv <arqamahmad805@gmail.com>
2025-11-13 01:05:34 +0500

    Delete user_progress directory

--- user_progress/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserCourse, QuizCourseProgress, QuizChapterProgress, QuizSubtopicProgress, QuizQuestionProgress, LatestSubmittedQuizAnalytics, LatestSubmittedTestAnalytics
-
-# Register your models here.
-admin.site.register([UserCourse, QuizCourseProgress, QuizChapterProgress, QuizSubtopicProgress, QuizQuestionProgress, LatestSubmittedQuizAnalytics, LatestSubmittedTestAnalytics])
